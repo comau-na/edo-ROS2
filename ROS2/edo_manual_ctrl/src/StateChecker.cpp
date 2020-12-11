6cce979d34ef1e5b959d6b55b9d3d883a7d161d2
Scott Howard <55260149+maclo4@users.noreply.github.com>
2020-12-11 09:33:19 -0500

    Update StateChecker.cpp

--- ROS2/edo_manual_ctrl/src/StateChecker.cpp
+++ ROS2/edo_manual_ctrl/src/StateChecker.cpp
@@ -45,72 +45,31 @@ int StateChecker::getState(){return machineState;}
 int StateChecker::getStateReceived(){return stateReceived;}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /*
-#include <functional>
-#include <memory>
+ROS1 code
+StateChecker::StateChecker(ros::NodeHandle& nh_in){
+  nh = nh_in;
+  machine_state_sub = nh.subscribe("/machine_state", 10,
+      &StateChecker::stateCallback, this);
+  stateReceived = false;
+}  // StateChecker::StateChecker()
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "edo_core_msgs/msg/machine_state.hpp"
-
-using std::placeholders::_1;
-
-class MinimalSubscriber : public rclcpp::Node
-{
-  public:
-    MinimalSubscriber()
-    : Node("minimal_subscriber")
-    {
-      subscription_ = this->create_subscription<edo_core_msgs::msg::MachineState>(
-        "machine_state", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-    }
+void StateChecker::stateCallback(const edo_core_msgs::MachineState& state){
+  stateReceived = true;
+  machine_state = state.current_state;
+}  // StateChecker::stateCallback()
 
 
-  private:
-    void topic_callback(const edo_core_msgs::msg::MachineState::SharedPtr msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->current_state);
-    }
-    rclcpp::Subscription<edo_core_msgs::msg::MachineState>::SharedPtr subscription_;
-   
-  };
 
-int main(int argc, char * argv[])
-{
-  edo_core_msgs::msg::MachineState msg;
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
-  return 0;
-}
+*/
+
+
+
+
+
+
+
+
+
+
 */
