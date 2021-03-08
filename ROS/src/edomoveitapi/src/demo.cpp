b6467ad68e3edf498296f3a8afd570a4e62c459a
WameedM <wameed98@gmail.com>
2021-03-07 22:04:26 -0500

    Demo moves robot arm to cube

--- ROS/src/edomoveitapi/src/demo.cpp
+++ ROS/src/edomoveitapi/src/demo.cpp
@@ -28,13 +28,13 @@ int main(int argc, char** argv)
 
   //set pose 
   geometry_msgs::Pose target_pose;
-  target_pose.position.x = 0.079;
-  target_pose.position.y = 0.2498;
-  target_pose.position.z = 2.01;
-  target_pose.orientation.x = -0.0382;
-  target_pose.orientation.y = 0.05455;
-  target_pose.orientation.z = 0.57782;
-  target_pose.orientation.w = 0.8134;
+  target_pose.position.x = -0.6847;
+  target_pose.position.y = 0.001898;
+  target_pose.position.z = 1.04094;
+  target_pose.orientation.x = -0.34125;
+  target_pose.orientation.y = -0.82208;
+  target_pose.orientation.z = 0.17362;
+  target_pose.orientation.w = 0.42132;
   move_group.setPoseTarget(target_pose);
   ROS_INFO_NAMED("move_to_pose", "Setting the target position to x=%g, y=%g, z=%g",target_pose.position.x, target_pose.position.y, target_pose.position.z);
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
