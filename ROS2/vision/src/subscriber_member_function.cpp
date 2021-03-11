79f66defe48290d58a4ea532c7a891164c53d7c4
Allen Lee Paul <fw1067@wayne.edu>
2021-03-10 20:40:11 -0500

    Reordered classes

--- ROS2/vision/src/subscriber_member_function.cpp
+++ ROS2/vision/src/subscriber_member_function.cpp
@@ -12,73 +12,134 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include <string>
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include "common.hpp"
 #include <functional>
 #include <memory>
-#include <blocks_buckets.h>
 #include <vector>
 #include "rclcpp/rclcpp.hpp"
-#include "vision_msgs/msg/detection2_d_array.hpp"
+ #include "vision_msgs/msg/detection2_d_array.hpp"
+ #include "geometry_msgs/msg/pose2_d.hpp"
+ #include "vision_msgs/msg/detection2_d.hpp"
 
 
 using std::placeholders::_1;
 //Subscriber nodes
 class EdoVision : public rclcpp::Node
 {
-public:
-  EdoObjDetection()
-  : Node("edo_ObjectDetection")
-  {
-    subscription_obj = this->create_subscription<vision_msgs::msg::Detection2DArray>(
+  public:
+    EdoVision()
+    : Node("EdoVision")
+    {
+      subscription_ = this->create_subscription<vision_msgs::msg::Detection2DArray>(
       "/detection", 10, std::bind(&EdoVision::detectionCallback, this, _1));
-  }
+    }
 
-  EdoClassification()
-  : Node("edo_Classification")
-  {
-     subscription_class = this->create_subscription<vision_msgs::msg::Classification2D>(
-      "/detection", 10, std::bind(&EdoVision::classificationCallback, this, _1));
-  }
+  private:
 
-private:
-  void detectionCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg) 
+  void detectionCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg)
   {
-    std::cout << "detection msg contents start: " << std::endl;
+    std::cout << "test" << std::endl;
     for(vision_msgs::msg::Detection2D detection : msg->detections){
       // just for testing. print out values
-      std::cout << "detection msg contents end: " << std::endl;
+      std::cout << "test" << std::endl;
     }    
 
   }
-   void classificationCallback(const vision_msgs::msg::Classification2D::SharedPtr msg) 
-  {
-    std::cout << "classifiation msg contents start: " << std::endl;
-    for(vision_msgs::msg::Classification2D classification : msg->results){
-      // just for testing. print out values
-      std::cout << "classifiation msg contents start:" << std::endl;
+ 
+  rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr subscription_;
 };
 
-//class1 or 2. 1 = Bucket, 2 = Block, 3 = Unknown?
-class Blocks
+class OBJ_Location : public rclcpp::Node
 {
-  int classification;
-  double block_curr_position;
-  std::string blockColor;
-  
-   Block( int classification, double curr_position, std::string blockColor)
+  public:
+    OBJ_Location()
+    : Node("OBJ_Location")
+    {
+      subscription_ = this->create_subscription<geometry_msgs::msg::Pose2D>(
+      "/detection", 10, std::bind(&OBJ_Location::OBJ_loc_Callback, this, _1));
+    }
+  private:
+    void OBJ_loc_Callback(const geometry_msgs::msg::Pose2D::ConstPtr msg)
+  {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->x.c_str());
+
+  }
+ 
+   rclcpp::Subscription<vision_msgs::msg::Detection2DArray>::SharedPtr subscription_;
+};
+
+
+//class1 or 2. 1 = Bucket, 2 = Block, 3 = Unknown?
+class Block
+{
+  public:
+
+  std::string classification;
+  float block_center_x;
+  float block_center_y;
+ 
+    Block( std::string classification, float block_center_x, float block_center_y)
   {
     this->classification = classification;
-    this->block_curr_position = curr_position;
-    this->std::string blockColor;
+    this->block_center_x = block_center_x;
+    this->block_center_y = block_center_y;
   }
 
-  boolean isBlock(int classification)
+  std::string getClassification(Block block)
   {
-    if(classification == 2)
+    return classification;
+  }
+
+   bool isBlock(vision_msgs::msg::Detection2D msg)
+  {
+    std::string result;
+    msg.tracking_id = result;
+    if (result == "cube" )
+    {
+      return true;
+    }
+    else
+    {
+    return false;
+  }
+
+  void setBlockValues(geometry_msgs::msg::Pose2D msg, Block block)
+  {
+    msg.x = block.block_center_x;
+    msg.y = block.block_center_y;
+  }
+
+   void setBlockClassification(vision_msgs::msg::Detection2D msg, Block block)
+  {
+    msg.tracking_id = block.classification;
+  }
+
+};
+
+class Bucket
+{
+  public:
+
+  std::string classification;
+  float bucket_center_x;
+  float bucket_center_y;
+ 
+    Bucket( std::string classification, float bucket_center_x, float bucket_center_y)
+  {
+    this->classification = classification;
+    this->bucket_center_x = bucket_center_x;
+    this->bucket_center_y = bucket_center_y;
+  }
+
+  std::string getClassification(Bucket bucket)
+  {
+    return classification;
+  }
+
+   bool isBucket(vision_msgs::msg::Detection2D msg)
+  {
+    std::string result;
+    msg.tracking_id = result;
+    if (result == "bucket" )
     {
       return true;
     }
@@ -88,171 +149,89 @@ class Blocks
     }
   }
 
-  double getBlockPosition()
-  {
-    return curr_position;
-  }
   
-  void setBlockPostion( int p)
+  void setBucketValues(geometry_msgs::msg::Pose2D msg, Bucket bucket)
   {
-    p = curr_position;
+    msg.x = bucket.bucket_center_x;
+    msg.y = bucket.bucket_center_y;
   }
 
-  std::string getBlockColor()
+  void setBucketClassification(vision_msgs::msg::Detection2D msg, Bucket bucket)
   {
-    return blockColor;
+    msg.tracking_id = bucket.classification;
   }
 
-  setBlockColor(std::string c)
-  {
-    c = blockColor;
-  }
 
 };
 
-class Buckets
+
+void readEnvironment()
 {
-  int classification;
-  double bucket_curr_position;
-  std::string blockColor;  
-
-
-  Bucket( int classification, double curr_position, std::string bucketColor)
-  {
-    this->classification = classification;
-    this->curr_position = curr_position;
-    this->std::string blockColor; 
-  }
-
- boolean isBucket( int classification)
- {
-   if(classification == 1)
-   {
-     return true;
-   }
-   else 
-   {
-     return false;
-   }
- }
-
-  double getBucketPosition()
-  {
-    return curr_position;
-  }
-
-  setBucketPosition(double pos)
-  {
-    pos = curr_position;
-  }
-
- std::string getBucketColor()
-  {
-    return bucketColor;
-  }
-
-  setBucketColor(std::string col)
-  {
-    col = bucketColor;
-  }
-};
-
-
-
-class EdoSorting
-{
-public:
-
-   //to save Buckets
-  std::vector<Blocks> BucketVector;
-  //to save Blocks
-  std::vector<Buckets> BlockVector;
-
-  void readEnvironment(int classification)
-  
-  {    
-    //if the camera has detected something// cant use this in if
-    if(vision_msgs::msg::Detection2D detection : msg->detections =! NULL)
-    {
-      //loop thru detection array
-      for (i = 0, i<vision_msgs::msg::Detection2D detection : msg->detections; i++)
-      {
-        //check if Block or bucket
-        if(isBlock()==true)
-        {
-          //if is block, create block obj
-          //get block location
-          //get block classification
-          Blocks block;
-          block.setBlockPositon(i);
-          for(j = 0; j < vision_msgs::msg::Classification2D classification : msg->results)
-          {
-            block.setBlockColor
-          }
-          else{
-             //if(detection == bucket)
-            // create bucket obj
-            //get bucket location
-            //get bucket classfication
-            Buckets bucket;
-            bucket.setBucketPositon(i);
-            for(j = 0; j < vision_msgs::msg::Classification2D classification : msg->results)
-              {
-                bucket.setBucketColor
-              }
-
-          }
-        }
-      
-      }
-     
-
-    }
-  };
-
-
-  void ExecuteCommand(std::vector BucketVector, std::vector BlockVector)
-  {
-     //take in all buckets, blocks from readEnvironment
-   
-     //recieve block current postion, bucket postion is declared as destination coordinate(center of bucket) if block is not inside bucket
-      //matches block and bucket classes
-      //send messege to movement wrapper to navigate block to bucket destination
-      //gripper logic
+ //check if the camera has detected something
  
-  }
+      //if so, loop thru detection array
+      int i = 0;
+for(i = 0; i < vision_msgs::msg::Detection2DArray msg.detections; i++)
+    {
+        //if is block, create block obj
+        if(isBlock() == true)
+        {
+           Block newblock;
+            newblock.setBlockValues();
+            newblock.setBlockClassification();
+        }
+
+        if(isBucket() == true)
+        {
+         Bucket newbucket;
+         newbucket.setBucketValues;
+         newbucket.setBucketClassification;
+        }
+       
+       //store blocks into block vector
+       //store buckets into bucket vector
+         
+       
+    }
+}
 
 
-};
 
 
+//take in all buckets, blocks from readEnvironment
+//recieve block current postion, bucket postion is declared as destination coordinate(center of bucket) if block is not inside bucket
+//matches block and bucket classes
+//send messege to movement wrapper to navigate block to bucket destination
+//gripper logic
+
+void ExecuteCommand()
 
 
 int main(int argc, char * argv[])
 {
-  
+ 
 
   rclcpp::init(argc, argv);
   rclcpp::executors::SingleThreadedExecutor exec;
   rclcpp::NodeOptions options;
   //auto subscriber_node = std::make_shared<EdoVision>();
 
-  rclcpp::spin(std::make_shared<EdoObjDetection>());
+  rclcpp::spin(std::make_shared<EdoVision>());
   //exec.add_node(subscriber_node);
-   rclcpp::spin(std::make_shared<EdoClassification>());
+  //  rclcpp::spin(std::make_shared<OBJ_Location>());
   //exec.add_node(subscriber_node);
-
   std::chrono::nanoseconds timeout;
   timeout= std::chrono::nanoseconds { 100000000 };
 
   /*
   while(rclcpp::ok()){
     exec.spin_some();
-    
+   
     //std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    
+   
   }
   */
   rclcpp::shutdown();
   return 0;
 };
+
