317cf445ace7da88eee62428a89fdba9ab12c62b
SunnyPrajapati-gh6740 <55172654+SunnyPrajapati-gh6740@users.noreply.github.com>
2021-04-01 18:28:19 -0400

    Add files via upload
    Subscriber for pulling ROS Image to CV

---
+++ ROStoCV.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import os
+import numpy as np
+
+class ROStoCV(object):
+    def __init__(self):
+        # Params
+        self.image = None
+        self.br = CvBridge()
+        self.loop_rate = rospy.Rate(1)
+
+        # Publishers
+        #self.pub = rospy.Publisher('imagetimer', Image,queue_size=10)
+
+        # Subscribers
+        rospy.Subscriber("edo/camera/image_raw",Image,self.callback)
+
+    def callback(self, msg):
+        rospy.loginfo('Image received...')
+        self.image = self.br.imgmsg_to_cv2(msg)
+	
+
+    def start(self):
+        rospy.spin()
+        while not rospy.is_shutdown():
+            rospy.loginfo('publishing image')
+            br = CvBridge()
+            #if self.image is not None:
+             #   self.pub.publish(br.cv2_to_imgmsg(self.image))
+            self.loop_rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node("imagetimer111", anonymous=True)
+    my_node = ROStoCV()
+    my_node.start()
