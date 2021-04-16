a8937e3e06f23dec449861cf4a0e443f6cd04fbd
Allen Lee Paul <62396502+AllenPaulgh@users.noreply.github.com>
2021-04-16 13:14:31 -0400

    publisher revisions

--- ROS2/edo_navigation/edo_navigation/edo_navigation/edo_environment_subscriber.py
+++ ROS2/edo_navigation/edo_navigation/edo_navigation/edo_environment_subscriber.py
@@ -2,6 +2,8 @@
 
 import rclpy
 
+import time 
+
 import numpy
 
 from rclpy.node import Node
@@ -10,13 +12,23 @@ from geometry_msgs.msg import Pose
 
 from std_msgs.msg import Bool
 
-from edo_navigation.edo_scan import image_classifier,image_converter
+from edo_navigation.edo_scan import image_classifier, image_converter
+
+
+
+
 
 msg = None
 recievedImage = False
 angle = 0
 
-
+class Detection:
+    def __init__(self, classification, coordinateCenter, confidence, box, angle):
+        self.classification = classification
+        self.coordinateCenter = coordinateCenter
+        self.confidence = confidence
+        self.box = box
+        self.angle = angle
 
 class navigation_publisher(Node):
     def __init__(self):
@@ -34,13 +46,14 @@ class navigation_publisher(Node):
         msg._orientation.z = o_z
         msg._orientation.w = o_w
         self.publisher_.publish(msg)
+        
 
  
         
 class gripper_publisher(Node):
     def __init__(self):
         super().__init__('gripper')
-        self.publisher = self.create_publisher(Bool,'gripper',10)
+        self.publisher = self.create_publisher(Bool,'open_gripper',10)
  
     
     def open_close(self, input):
@@ -59,26 +72,11 @@ class Cube:
         self.classification = classification
         self.angle = angle
 
-    def setAngle(self,angle):
-        self.angle = angle
-        
-
-    def setCubeValues(self,coordinateCenter):
-        self.coordinateCenter = coordinateCenter
-    
-    def setClassification(self,classification):
-        self.classification = classification
 
 class Bucket:
     def __init__(self,center_x,center_y, classification):
         self.center_x = center_x
         self.center_y = center_y
-        self.classification = classification
-
-    def setBucketValues(self,coordinateCenter):
-        self.coordinateCenter = coordinateCenter
-    
-    def setClassification(self,classification):
         self.classification = classification
 
 
@@ -92,49 +90,9 @@ def colorCheck(Cube,Bucket) :
     return(bucket_color_classification == cube_color_classification)
     
     
-def isCube(self,classification):
-    result = self.classification.split('_')[0]
+def isCube(obj):
+    result = obj.classification.split('_')[0]
     return(result == "cube")
-
-def isBucket(self):
-    result = self.classification.split('_')[0]
-    return(result == "bucket")
-
-Bucket_list = []
-Cube_list = []
-
-def readEnvironment():
-
-    
-    classifier =  image_classifier()
-    detection_array = classifier.detections
-    print(detection_array)
-    for obj in detection_array:
-        print(isCube(obj))
-        if isCube(obj) == True:
-            print('Cube detected')
-            new_Cube = Cube()
-            new_Cube.setCubeValues(obj.coordinateCenter) 
-            new_Cube.setClassification(obj.classification)
-            new_Cube.setAngle(obj.angle)
-            Cube_list.append(new_Cube)
-            
-        if isBucket(obj) == True:
-            print('bucket detected')
-            new_bucket = Bucket()
-            new_bucket.setBucketValues(obj.coordinateCenter)
-            new_bucket.setClassification(obj.classification)
-            Bucket_list.append(new_bucket)
-            print(Cube_list,Bucket_list)
-            return(Bucket_list,Cube_list)
-
-
-# take in all buckets, Cubes from readEnvironment lists
-# recieve Cube and bucket positions
-# loop through Cube list, guide robot to Cube location
-# pick up Cube
-# guide robot to corresponding bucket
-# repeat until no Cubes
 
 
 
@@ -142,59 +100,64 @@ def executeCommand(Cube_list, Bucket_list):
     #loop through Cube list 
     pub = navigation_publisher()
     grip = gripper_publisher()
-
+    
     for cube in Cube_list:
         for bucket in Bucket_list:
-            if(colorCheck(cube,bucket) == True):
-                print(colorCheck(cube,bucket))
+            if colorCheck(cube,bucket) == True:
                 print("starting navigation")
                 #start navigation
-                
+                #initialize coordinates
                 p_x = 0
                 p_y = 0
                 p_z = 0
                 o_x = 0
                 o_y = 0
                 o_z = 0
-                o_w = 0 
-                #initialize coordinates
+                o_w = float(0) 
+                
                 #starting position
+                
                 p_x = -0.413271964035
                 p_y = 0.000747108786946
                 p_z = 1.20756253284
                 o_x = -3.139559138676179
                 o_y = -0.015483082006265326
                 o_z = -0.7787520820355616
-                o_w = 62.00000
+                o_w = float(62.00000)
                 pub.publish_coordinates(p_x,p_y,p_z,o_x,o_y,o_z,o_w)
+                
 
                 #cube position (hover)
-                p_x = cube.coordinateCenter[1]
-                p_y = cube.coordinateCenter[2]
+                
+                p_x = cube.center_x
+                p_y = cube.center_y
                 p_z = 1.10756253284
                 o_x = -3.139559138676179
                 o_y = -0.015483082006265326
-                o_z = numpy.pi/4 - numpy.pi/2 + angle
-                o_w = 62
+                o_z = numpy.pi/4 - numpy.pi/2 + cube.angle
+                o_w = float(62)
                 pub.publish_coordinates(p_x,p_y,p_z,o_x,o_y,o_z,o_w)
+        
 
                 #gripper open
-                input = 0.35
-                gripper_publisher(input)
+                
+                input = True
+                grip.open_close(input)
                 #cube position (grab)
                 
-                p_x = cube.coordinateCenter[1]
-                p_y = cube.coordinateCenter[2]
+                
+                p_x = cube.center_x
+                p_y = cube.center_y
                 p_z = 1.04000
                 o_x = -3.139559138676179
                 o_y = -0.015483082006265326
-                o_z = numpy.pi/4 - numpy.pi/2 + angle
-                o_w = 62
+                o_z = numpy.pi/4 - numpy.pi/2 + cube.angle
+                o_w = float(62)
                 pub.publish_coordinates(p_x,p_y,p_z,o_x,o_y,o_z,o_w)
 
 
                 #gripper close
-                input = 0.10
+                input = False
                 grip.open_close(input)
 
                 # #cube position (post-grab)
@@ -203,53 +166,133 @@ def executeCommand(Cube_list, Bucket_list):
                 p_z = 1.20756253284
                 o_x = -3.139559138676179
                 o_y = -0.015483082006265326
-                o_z = numpy.pi/4 - numpy.pi/2 + angle
-                o_w = 62
+                o_z = numpy.pi/4 - numpy.pi/2 + cube.angle
+                o_w = float(62)
                 pub.publish_coordinates(p_x,p_y,p_z,o_x,o_y,o_z,o_w)
 
                 # #bucket position hover
-                p_x = bucket.coordinateCenter[1]
-                p_y = bucket.coordinateCenter[2]
+                p_x = bucket.center_x
+                p_y = bucket.center_y
                 p_z = 1.180000
                 o_x = -3.139559138676179
                 o_y = -0.015483082006265326
-                o_z = numpy.pi/4 - numpy.pi/2 + angle
-                o_w = 62
+                o_z = numpy.pi/4 - numpy.pi/2 + cube.angle
+                o_w = float(62)
                 pub.publish_coordinates(p_x,p_y,p_z,o_x,o_y,o_z,o_w)
 
                  #gripper open
-                input = 0.35
+                input = True
                 grip.open_close(input)
 
-
+                rclpy.shutdown()
         # start navigation from current Cube coordinates to destination coordinates
         
 
     
       
 def main(args=None):
+
+    
+    Bucket_list = []
+    Cube_list = []
+    detection_array = []
+
     rclpy.init(args=args)
     global msg
     ic = image_converter()
 
     while rclpy.ok() and ic.recievedImage is False:
         rclpy.spin_once(ic)
+
     msg = ic.msg 
     
     #getbase here takes in raw image
     
-
+    #save detections within detection array
     classifier = image_classifier()
     classifier.classify_objects(msg)
-
-    readEnvironment()
+    for obj in classifier.detections:
+        detection_array.append(obj)    
+    print("detection array contains: ", detection_array)
+    for obj in detection_array:
+        if isCube(obj) == True:
+            print(obj.classification)
+            new_bucket = Bucket(center_x = float(obj.coordinateCenter[0]), 
+                                center_y = float(obj.coordinateCenter[1]),
+                                classification= obj.classification)
+            Bucket_list.append(new_bucket)
+            print("cube", isCube(obj))
+        if isCube(obj) == False:
+            print(obj.classification)
+            print("bucket",isCube(obj))
+            new_Cube = Cube(center_x = float(obj.coordinateCenter[0]),
+                            center_y = float(obj.coordinateCenter[1]),
+                            classification= obj.classification,
+                            angle= obj.angle
+                            )
+            Cube_list.append(new_Cube)
+            
+    #take in empty cubelist,bucketlist,detection array
+    print("cubes: ", Cube_list)
+    print("buckets: ", Bucket_list)
     
-     
-    executeCommand(Cube_list,Bucket_list)    
+    print("start read environment")
+    # readEnvironment(detection_array, Bucket_list, Cube_list)    
+    #take in cubelist, bucketlist 
+    print("start executeCommand")
+    while rclpy.ok():
+        navpublisher = navigation_publisher()
+        
+        for cube in Cube_list:
+            for bucket in Bucket_list:
+                if colorCheck(cube,bucket) == True:
+                    #start navigation
+                #initialize coordinates
+                    p_x = float(0)
+                    p_y = float(0)
+                    p_z = float(0)
+                    o_x = float(0)
+                    o_y = float(0)
+                    o_z = float(0)
+                    o_w = float(0) 
+                
+                
+                    #starting position
+                
+                    p_x = -0.413271964035
+                    p_y = 0.000747108786946
+                    p_z = 1.20756253284
+                    o_x = -3.139559138676179
+                    o_y = -0.015483082006265326
+                    o_z = -0.7787520820355616
+                    o_w = float(62.00000)
+                    navpublisher.publish_coordinates(p_x,p_y,p_z,o_x,o_y,o_z,o_w)
+                    time.sleep(10)
+                    print("first move finished")
 
-    
-    # rclpy.destroy_node(gripper_publisher)
+                    #cube hover 
+                    p_x = float(cube.center_x)
+                    p_y = float(cube.center_y)
+                    p_z = 1.10756253284
+                    o_x = -3.139559138676179
+                    o_y = -0.015483082006265326
+                    o_z = numpy.pi/4 - numpy.pi/2 + cube.angle
+                    o_w = float(62)
+                    navpublisher.publish_coordinates(p_x,p_y,p_z,o_x,o_y,o_z,o_w)
+                    time.sleep(10)
+                    print("second move finished")
+
+
+
+
+
+
+
     rclpy.shutdown()
+
+       
+    
+    
     
 
 if __name__ == '__main__':
